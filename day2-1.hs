e15ff0ed312005cb219e8fcc26d366c564dfe011
CatCraftYT <37033401+CatCraftYT@users.noreply.github.com>
2024-12-03 23:16:13 +1030

    Added day 2 part 1 solution

---
+++ day2-1.hs
@@ -0,0 +1,30 @@
+import Data.List.Split (splitOn)
+
+isIncreasing :: [Int] -> Bool
+isIncreasing [x,y] = x > y
+isIncreasing (x:y:ys) = (x > y) && isIncreasing (y:ys)
+
+isDecreasing :: [Int] -> Bool
+isDecreasing [x,y] = x < y
+isDecreasing (x:y:ys) = (x < y) && isDecreasing (y:ys)
+
+isMonotonic :: [Int] -> Bool
+isMonotonic xs = isIncreasing xs || isDecreasing xs
+
+isChangingSlowly :: [Int] -> Bool
+isChangingSlowly [x,y] = pred
+    where diff = abs (x - y)
+          pred = diff <= 3 && diff >= 1
+isChangingSlowly (x:y:ys) = pred && isChangingSlowly (y:ys)
+    where diff = abs (x - y)
+          pred = diff <= 3 && diff >= 1
+
+isSafe :: [Int] -> Bool
+isSafe xs = isChangingSlowly xs && isMonotonic xs
+
+parseLine :: String -> [Int]
+parseLine xs = map read $ splitOn " " xs
+
+main = do
+    contents <- readFile "inputs/day2.txt"
+    print.length $ filter id [isSafe.parseLine $ line | line <- lines contents]
