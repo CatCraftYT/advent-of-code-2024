ebc50229edb3d4f89949707f4cd5171ca4f54672
CatCraftYT <37033401+CatCraftYT@users.noreply.github.com>
2024-12-06 01:22:34 +1030

    Added day 5 part 1 solution

---
+++ day5-1.hs
@@ -0,0 +1,39 @@
+import Data.List.Extra (splitOn, sortBy)
+
+data Rule = Rule Int Int deriving (Show)
+type Page = [Int]
+
+rfst :: Rule -> Int
+rfst (Rule a _) = a
+
+rsnd :: Rule -> Int
+rsnd (Rule _ b) = b
+
+compareUsingRules :: [Rule] -> Int -> Int -> Ordering
+compareUsingRules [] _ _ = EQ
+compareUsingRules (r:rs) a b
+    | rfst r == a && rsnd r == b = LT
+    | rsnd r == a && rfst r == b = GT
+    | otherwise = compareUsingRules rs a b
+
+parseRule :: String -> Rule
+parseRule s = Rule (head broken) (last broken)
+    where broken = read <$> splitOn "|" s :: [Int]
+
+parseContents :: [[String]] -> ([Rule], [Page])
+parseContents contents = (map parseRule ruleList, map (map read . splitOn ",") pagesList)
+    where ruleList = head contents
+          pagesList = last contents
+
+sortPages :: [Rule] -> [Page] -> [Page]
+sortPages rs = map (sortBy (compareUsingRules rs))
+
+filterGoodPages :: [Rule] -> [Page] -> [Page]
+filterGoodPages rs ps = map fst . filter (uncurry (==)) . zip ps $ sortPages rs ps
+
+median :: [a] -> a
+median l = l !! (length l `div` 2)
+
+main = do
+    (rules, pages) <- parseContents . map lines.splitOn "\n\n" <$> readFile "inputs/day5.txt"
+    print $ sum.map median $ filterGoodPages rules pages
