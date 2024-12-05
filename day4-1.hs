4b3eb2271c062bc51b0efde7995a90c17d18afbf
CatCraftYT <37033401+CatCraftYT@users.noreply.github.com>
2024-12-05 15:30:56 +1030

    Added incorrect 4-1 solution

--- day4-1.hs
+++ day4-1.hs
@@ -1,4 +1,4 @@
-import Data.List.Extra ( transpose, (!?) )
+import Data.List.Extra ( transpose, (!?), isInfixOf )
 import Data.Maybe ( catMaybes )
 
 getRows :: [String] -> [String]
@@ -13,4 +13,21 @@ getLeftDiagonals m = [catMaybes [m !! i !? (i + offset) | i <- rows] | offset <-
           offsets = [(negate.length) m + 1 .. (length.head) m - 1]
 
 getRightDiagonals :: [String] -> [String]
-getRightDiagonals m = 
+getRightDiagonals m = [(reverse.catMaybes) [m !! row !? (col - row) | row <- rows] | col <- cols]
+    where nRows = length m - 1
+          nCols = (length.head) m - 1
+          rows = [0..nRows]
+          cols = [0..nRows + nCols]
+
+getAllDirections :: [String] -> [String]
+getAllDirections m = getRows m ++ getCols m ++ getLeftDiagonals m ++ getRightDiagonals m
+
+filterWordInstances :: String -> [String] -> [String]
+filterWordInstances word words = matches ++ backwardsMatches
+    where matches = filter (word `isInfixOf`) words
+          backwardsMatches = filter (reverse word `isInfixOf`) words
+
+main = do
+    contents <- lines <$> readFile "inputs/day4.txt"
+    print $ (length . filterWordInstances "XMAS" . getAllDirections) contents
+    print $ (filterWordInstances "XMAS" . getAllDirections) contents
